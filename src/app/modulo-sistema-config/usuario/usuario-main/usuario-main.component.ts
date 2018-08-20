0334f5292dc0561b64e9b48713982749a5f06189
Luis Rojas Puerta <ariesluis_12@hotmail.com>
2018-08-20 18:16:53 -0500

    Usuarios Validacion 2

--- src/app/modulo-sistema-config/usuario/usuario-main/usuario-main.component.ts
+++ src/app/modulo-sistema-config/usuario/usuario-main/usuario-main.component.ts
@@ -1,31 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'ad-usuario-main',
+  selector: 'app-usuario-main',
   templateUrl: './usuario-main.component.html',
   styleUrls: ['./usuario-main.component.css']
 })
 export class UsuarioMainComponent implements OnInit {
-  show: boolean;
 
+  public titulo:string = "Usuarios"
   constructor() { }
-
   ngOnInit() {
-  }
-
-
-  onActivate() {
-    console.log("Activate outlet main");
-   
-   
-
-  }  
-
-  onDeactivate() {
-    console.log("Deactivate outlet main");
-    this.show = true;
-   
-
   }
 
 }
--- src/app/modulo-sistema-config/usuario/usuario-model.ts
+++ src/app/modulo-sistema-config/usuario/usuario-model.ts
@@ -1,24 +1,17 @@
 import { PerfilModel } from "../perfil/perfil-model";
 import { FilialModel } from "../filial/filial-model";
-import { UsuarioEmpleadoModel } from "./usuario-empleado-model";
-
-
 export class UsuarioModel {
-
     constructor(
         public idusuario:number = 0,
         public nomusuario:string = null,
         public dni:string = null,
         public login:string = null,
         public clave:string = null,
-        public activo:number = null,
+        public activo:boolean = true,
         public perfil:PerfilModel = null,
         public filial:FilialModel = null,
-        public usuarioempleados: UsuarioEmpleadoModel[] = [],
         public status:any = null
-        
-
     ){
-
     }
 }
+
